c6c914f907ba51764157a77669351a6d3789ada8
KIyanZ <alundu008@ieselcaminas.org>
2022-09-27 19:42:38 +0200

    DONE

--- src/main/kotlin/Ejercicios/Exercici_1.kt
+++ src/main/kotlin/Ejercicios/Exercici_1.kt
@@ -8,10 +8,10 @@ fun main(args: Array<String>) {
     //Root Directory
     val root = File.listRoots()[0];
     var f = root;
-    val input = BufferedReader(InputStreamReader(System.`in`))
-    var option = "0"
+    BufferedReader(InputStreamReader(System.`in`))
+    var option = 0
 
-    while (option != "-1") {
+    while (option != -1) {
         printList(f)
         println(
             "Introduce a number: \n (0 to go to parent directory) \n" +
@@ -19,41 +19,49 @@ fun main(args: Array<String>) {
                     " (-1 to end program)"
         )
         System.out.println("---------------------------------------------------")
-
-        val input = BufferedReader(InputStreamReader(System.`in`)).readLine()
-        if (input.equals("0")) {
-            if (f.exists() && f.isDirectory()) {
-                if (goToParentDir(f)) {
-                    f = f.parentFile;
+        val input = BufferedReader(InputStreamReader(System.`in`)).readLine().toInt()
+        if (input >= 0 && input <= f.listFiles().sorted().lastIndex + 1) {
+            if (input == 0) {
+                    if (goToParentDir(f)) {
+                        f = f.parentFile;
+                    }
+            } else {
+                val f2 = f.listFiles().sorted().get(input - 1)
+                if (f2.exists() && f2.isDirectory){
+                    if (f2.canRead())
+                        f = f2 else println("You don't have the read permission of this Directory")
+                } else {
+                    println("Not a directory")
                 }
-            } else
-                println("Not a directory")
-        } else {
-            println("Directory does not exist")
 
-        }
+            }
+        } else println("Please input number between valid range")
+        if (input == -1) option = input
     }
 }
 
-
 fun printList(f: File) {
-    var num = 0;
-    System.out.println("List of file and directories in " + f.path)
-    System.out.println("---------------------------------------------------")
+    var num = 1;
+    println("List of file and directories in " + f.path)
+    println("---------------------------------------------------")
+    println("0.- Directori Pare")
     for (e in f.listFiles().sorted()) {
-        System.out.println("$num.- $e");
+        if (e.isDirectory){
+            println("$num.- ${e.name}  <Directory>")
+        } else println("$num.- ${e.name}   ${e.length()}")
         num++;
     }
 }
 
-fun goToParentDir(f: File): Boolean {
-    if (f.parent == null) {
+fun goToParentDir(fAUX: File): Boolean {
+    if (fAUX.parentFile == null) {
         println("Can't go to parent directory cah you're in the root directory")
         return false;
     } else {
         return true;
     }
 }
+
 
 
 
